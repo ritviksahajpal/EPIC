3da231a41a40584ff5632274fde798ebe6b1e403
ritvik sahajpal <ritvik@umd.edu>
2015-09-28 21:57:20 -0400

    minor tweaks: PEP8ify, removed visual studio files

--- read_EPIC_output/read_EPIC_output.py
+++ read_EPIC_output/read_EPIC_output.py
@@ -1,4 +1,4 @@
-import constants, pandas, pdb, os, fnmatch
+import constants, pandas, pdb, os, fnmatch, logging
 from datetime import datetime, timedelta
 
 def parse_ACY(list_fls):
@@ -10,27 +10,23 @@ def parse_ACY(list_fls):
 
 def parse_ANN(list_fls):
     for fl in list_fls:
-        df      = pandas.read_csv(fl,skiprows=constants.SKIP,skipinitialspace=True,\
-                                  usecols=constants.ANN_PARAMS,sep=' ')
-        time_df = df[(df.YR>=constants.START_YR) & (df.YR<=constants.END_YR)]
+        df = pandas.read_csv(fl, skiprows=constants.SKIP, skipinitialspace=True, usecols=constants.ANN_PARAMS, sep=' ')
+        time_df = df[(df.YR >= constants.START_YR) & (df.YR <= constants.END_YR)]
         time_df.groupby(lambda x: xrange.year).sum()
 
 def parse_DGN(list_fls):
     for fl in list_fls:
-        df      = pandas.read_csv(fl,skiprows=constants.SKIP,delim_whitespace=True,usecols=constants.DGN_PARAMS,
-                             parse_dates={"datetime": [0,1,2]}, index_col="datetime",
-                            date_parser=lambda x: pandas.datetime.strptime(x, '%Y %m %d'))    
+        df      = pandas.read_csv(fl, skiprows=constants.SKIP, delim_whitespace=True, usecols=constants.DGN_PARAMS,
+                                  parse_dates={"datetime": [0,1,2]}, index_col="datetime",
+                                  date_parser=lambda x: pandas.datetime.strptime(x, '%Y %m %d'))
         time_df = df.ix[constants.START_YR:constants.END_YR]        
         print time_df.groupby(time_df.index.map(lambda x: x.year)).max()
-        pdb.set_trace()
-
-
 
 def collect_epic_output():
     epic_fl_types = constants.GET_PARAMS
 
     for fl_name in epic_fl_types:
-        list_fls = fnmatch.filter(os.listdir(constants.sims_dir+os.sep),'*.'+fl_name)
+        list_fls = fnmatch.filter(os.listdir(constants.sims_dir + os.sep), '*.' + fl_name)
 
         if(fl_name == 'DGN'):
             parse_DGN(list_fls)
@@ -38,19 +34,8 @@ def collect_epic_output():
             parse_ACY(list_fls)
         elif(fl_name == 'ANN'):
             parse_ANN(list_fls)
-        elif(fl_name == 'ACN'):
-            parse_ACN(list_fls)
-
-    #in_ssurgo_file = dir_name+os.sep+constants.SPATIAL+os.sep+\
-    #            fnmatch.filter(os.listdir(dir_name+os.sep+constants.SPATIAL+os.sep),\
-    #            'soilmu_a_*.shp')[0]
-    pass
+        else:
+            logging.info( 'Wrong file type')
 
 if __name__ == '__main__':
     collect_epic_output()
-
-
-
-#print df.index
-#print df.head()
-pdb.set_trace()
