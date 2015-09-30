ebd5662445a33d24647cda518c8e2086e168f01c
ritvik sahajpal <ritvik@umd.edu>
2015-09-30 13:02:43 -0400

    minor tweaks

--- NARR/NARR_to_EPIC.py
+++ NARR/NARR_to_EPIC.py
@@ -88,6 +88,8 @@ def parallelize_NARR_to_EPIC():
     epic_wth_list = open(constants.out_dir+constants.EPIC_DLY,'r').readlines()
 
     if constants.DO_PARALLEL:
+        # Example: Split 441     "daily//20_15.txt"    46.405    -90.606
+        # to get lat_vals = 20, lon_vals = 15
         lat_vals = [int(ln.split('//')[1].split('.')[0].split('_')[0]) for ln in epic_wth_list]
         lon_vals = [int(ln.split('//')[1].split('.')[0].split('_')[1]) for ln in epic_wth_list]
 
