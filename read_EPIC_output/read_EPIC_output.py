7921c6f881ccd768ed88fa4265799795bc966ce8
ritvik sahajpal <ritvik@umd.edu>
2015-10-12 08:38:53 -0400

    ANN code is now being read propoerly

--- read_EPIC_output/read_EPIC_output.py
+++ read_EPIC_output/read_EPIC_output.py
@@ -10,30 +10,36 @@ class EPIC_Output_File():
         """
         Constructor
         """
+        # Get name of latest output directory (based on what time it was modified)
+        os.chdir(constants.epic_dir+os.sep+'output')
+        dirs = [d for d in os.listdir(constants.epic_dir+os.sep+'output') if os.path.isdir(constants.epic_dir+os.sep+'output')]
+        self.ldir = sorted(dirs, key=lambda x: os.path.getmtime(x), reverse=True)[:1][0]
+        self.epic_out_dir = constants.epic_dir + os.sep + 'output' + os.sep + self.ldir # Latest output directory
+
         # Create a sqlite database in the analysis directory
-        self.db_name = 'sqlite:///' + constants.anly_dir + os.sep + ftype + '_' + tag + '.db'
+        self.db_name = 'sqlite:///' + constants.anly_dir + os.sep + ftype + '_' + tag + '_' + self.ldir + '.db'
         self.engine  = create_engine(self.db_name)
         self.ftype   = ftype
         self.tag     = tag
 
-        # Get name of latest output directory (based on what time it was modified)
-        os.chdir(constants.epic_dir+os.sep+'output')
-        dirs = [d for d in os.listdir(constants.epic_dir+os.sep+'output') if os.path.isdir(constants.epic_dir+os.sep+'output')]
-        self.epic_out_dir = constants.epic_dir + os.sep + 'output' + os.sep + \
-                            sorted(dirs, key=lambda x: os.path.getmtime(x), reverse=True)[:1][0] # Latest output directory
-
     def parse_ACY(self, fl):
-        pdb.set_trace()
-        df      = pandas.read_csv(self.epic_out_dir + os.sep + self.ftype + os.sep + fl, skiprows=constants.SKIP,
-                                  skipinitialspace=True, usecols=constants.ACY_PARAMS, sep=' ')
+        pandas.read_csv(self.epic_out_dir + os.sep + self.ftype + os.sep + fl, skiprows=constants.SKIP,
+                        skipinitialspace=True, usecols=constants.ACY_PARAMS, sep=' ')
         time_df = df[(df.YR >= constants.START_YR) & (df.YR <= constants.END_YR)]
         time_df['site'] = fl[:-4]
         time_df.to_sql(self.db_name, self.engine, if_exists='append')
 
     def parse_ANN(self, fl):
-        df = pandas.read_csv(self.epic_out_dir + os.sep + self.ftype + os.sep + fl, skiprows=constants.SKIP, skipinitialspace=True,
-                             usecols=constants.ANN_PARAMS, sep=' ')
-        pdb.set_trace()
+        # Get column widths
+        cols_df  = pandas.read_table(self.epic_out_dir + os.sep + self.ftype + os.sep + fl, skiprows=constants.SKIP,
+                                     sep=' ', skipinitialspace=True)
+        widths = [5,4]
+        for i in range(len(cols_df.columns.values)-2):
+            widths.append(8)
+
+        df = pandas.read_fwf(self.epic_out_dir + os.sep + self.ftype + os.sep + fl, skiprows=constants.SKIP, sep=' ',
+                             skipinitialspace=True, widths=widths)
+
         time_df = df[(df.YR >= constants.START_YR) & (df.YR <= constants.END_YR)]
         time_df['site'] = fl[:-4]
         time_df.to_sql(self.db_name, self.engine, if_exists='append')
@@ -55,22 +61,20 @@ class EPIC_Output_File():
                                   skiprows=constants.SKIP, skipinitialspace=True, usecols=constants.ATG_PARAMS, sep=' ')
         time_df = df[(df.Y >= int(constants.START_YR)) & (df.Y <= int(constants.END_YR))]
         time_df['site'] = fl[:-4]
-
-        return fl[:-4], time_df.BIOM.max() - time_df.RWT.max()
-        #pdb.set_trace()
-        #time_df.to_sql(self.db_name, self.engine, if_exists='append')
+        time_df.to_sql(self.db_name, self.engine, if_exists='append')
+        #return fl[:-4], time_df.BIOM.max() - time_df.RWT.max()
 
     def collect_epic_output(self, fls):
         pool = Pool(constants.max_threads)
         if(self.ftype == 'DGN'):
-            self.parse_DGN(fls[0])
+            #self.parse_DGN(fls[0])
             pass #pool.map(self.parse_DGN, fls)
         elif(self.ftype == 'ACY'):
             #pool.map(self.parse_ACY, fls)
-            self.parse_ACY(fls[0])
+            pass #self.parse_ACY(fls[0])
         elif(self.ftype == 'ANN'):
             #pool.map(self.parse_ANN, fls)
-            pass #self.parse_ANN(fls[0])
+            self.parse_ANN(fls[0])
         elif(self.ftype == 'ATG'):
             pass #self.parse_ATG(fls[0])
             #pool.map(self.parse_ATG, fls)
@@ -78,13 +82,19 @@ class EPIC_Output_File():
             logging.info( 'Wrong file type')
 
     def sql_to_csv(self):
-        fls = ''
-        dfs = pandas.DataFrame(index=numpy.arange(0, len(fls)), columns=['site','biom_rwt'])
+        epic_fl_types = constants.GET_PARAMS
 
-        for idx, f in enumerate(fls):
-            st, val = self.parse_ATG(f)
-            dfs.loc[idx] = [st, val]
-        dfs.to_csv('C:\\Users\\ritvik\\Documents\PhD\\Projects\\Lake_States\\EPIC\\OpenLands_LS\\out_wi.csv')
+        for idx, fl_name in enumerate(epic_fl_types):
+            # Read in epic files
+            df = pandas.read_sql_table(self.db_name)
+            pass
+            fls = ''
+            dfs = pandas.DataFrame(index=numpy.arange(0, len(fls)), columns=['site','biom_rwt'])
+
+            for idx, f in enumerate(fls):
+                st, val = self.parse_ATG(f)
+                dfs.loc[idx] = [st, val]
+            dfs.to_csv('C:\\Users\\ritvik\\Documents\PhD\\Projects\\Lake_States\\EPIC\\OpenLands_LS\\out_wi.csv')
 
 if __name__ == '__main__':
     epic_fl_types = constants.GET_PARAMS
